815933363e9d6d01a34df9b3d8923fe9ceb83407
Calin Pristavu <calinpristavu@gmail.com>
2020-01-16 10:31:35 +0200

    Lib entry point

---
+++ src/CodeHighlight.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+class CodeHighlight
+{
+    public function highlight(string $string): string
+    {
+        // use the defined classes/services to perform the highlighting on $string
+
+        return $string;
+    }
+}
